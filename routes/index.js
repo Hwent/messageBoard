d5b133d0beef963d3d2a374c94e0a55bbea8d26d
Wentao <wentaowilliam@outlook.com>
2024-02-29 16:34:26 -0500

    Add feature create message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
 });
+
+// GET new message form
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "New Message" });
+});
+
+// POST new message
+router.post("/new", function (req, res, next) {
+  const messageText = req.body.message;
+  const messageUser = req.body.name;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect("/");
+});
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 
 module.exports = router;
